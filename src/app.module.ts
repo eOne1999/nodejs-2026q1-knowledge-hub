ed8b99642e17c34894397c7a293f4bb0093be990
eOne1999 <dmitriev_vanya@mail.ru>
2026-04-17 23:15:26 +0300

    feat: add throttler fot requests limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -21,6 +22,12 @@ import { RolesGuard } from './auth/guards/roles.guard';
     ArticleModule,
     CategoryModule,
     CommentModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [
